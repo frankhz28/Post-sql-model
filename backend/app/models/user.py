5cb0dd328b487a5e83fdd3982c4d1098cd1fd5da
frankhz28 <frank.hinojosa.z@uni.pe>
2026-03-11 13:14:42 -0500

    models/user.py implementado

---
+++ backend/app/models/user.py
@@ -0,0 +1,59 @@
+import re
+from pydantic import field_validator
+from datetime import datetime, timezone
+from enum import Enum
+from sqlmodel import SQLModel, Field
+
+class Role(str, Enum):
+    USER= "user"
+    EDITOR= "editor"
+    ADMIN= "admin"
+
+class UserBase(SQLModel):
+    email: str = Field(
+        max_length=255,
+        index=True,
+        unique=True
+    )
+    full_name: str = Field(
+        max_length=255
+    )
+
+class User(UserBase, table=True):
+    id: int = Field(
+        default=None,
+        primary_key=True,
+        index=True
+    )
+    hashed_password: str
+    role: Role = Field(
+        default=Role.USER
+    )
+    is_active: bool = Field(
+        default=True
+    )
+    created_at: datetime = Field(
+        default_factory= lambda: datetime.now(timezone.utc)
+    )
+
+
+class UserCreate(UserBase):    
+    password: str = Field(
+        description="Contraseña alpha numerico",
+        min_length=6,
+        max_length=72
+    )
+
+    @field_validator("password")
+    def validate_password_complexity(cls, value: str) -> str:
+        pattern = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9]+$"
+        if not re.match(pattern, value):
+            raise ValueError("El password debe contener al menos una letra, un numero y ser estrictamente alfanumerico")
+        return value
+
+class UserPublic(UserBase):
+    id : int
+    role : Role
+    is_active: bool
+    created_at: datetime
+    model_config= {"from_attributes": True}
